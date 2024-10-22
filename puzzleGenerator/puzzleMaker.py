b6ab8e41b9e26e7326deb27643e6519457d74250
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-10-22 10:51:57 +0200

    'Added documentation for sqlite-related functions'

--- puzzleGenerator/puzzleMaker.py
+++ puzzleGenerator/puzzleMaker.py
@@ -3,13 +3,16 @@ from random import choices
 import sqlite3
 import pandas as pd
 
+#Insert the name of the sqlite database here
+SQLITE_DATABASE_NAME = "sqlite.db"
+
+#Dimensions of the large and the small puzzles
 Ldimensions = {'x':13, 'y':6}
 Sdimensions = {'x':6, 'y':7}
 
 def loadDictionaryFR() -> set:
     """
     Loads the dictionary for the given language
-    @param language: string of the name of the language in lowercase
     @return: set containing all the words in the given dictionary
     """
     validWords = set()
@@ -47,38 +50,37 @@ def filterWords(allWords: set, letters: list, length=99, minlength = 3) -> set:
 
 def generatePuzzle(size: str) -> pd.DataFrame:
     """
-    JUST TESTING FOR NOW
-    @param size:
-    @return:
+    Generates a puzzle of the given size (usually 'S' or 'L')
+    @param size: character (usually 'S' or 'L') representing the type of puzzle
+    @return: pandas Dataframe containing the words, their direction and their starting coordinates
     """
     output = pd.DataFrame({'word': ["VACHE", "POULET"], 'IS_VERTICAL': [True, False], 'x': [3, 5], 'y':[4, 6]})
     return output
 
-def printDatabase():
+def printDatabase() -> None:
     """
-    JUST TESTING FOR NOW
-    @return:
+    prints out the puzzle and words database for testing purposes
+    @return: None
     """
     conn = sqlite3.connect('sqlite.db')
     c = conn.cursor()
     print(c.execute('SELECT * FROM puzzle').fetchall())
     print(c.execute('SELECT * FROM words').fetchall())
 
-def insertPuzzle(currentPuzzle: pd.DataFrame, puzzleType: str):
+def insertPuzzle(currentPuzzle: pd.DataFrame, puzzleType: str, database = SQLITE_DATABASE_NAME) -> None:
     """
-    JUST TESTING FOR NOW
-    @param currentPuzzle:
-    @param puzzleType:
-    @return:
+    Inserts a new puzzle into the database
+    @param currentPuzzle: dataframe containing the current puzzle
+    @param puzzleType: character (usually 'S' or 'L') representing the type of puzzle
+    @param database: path to the database, equal to the name specified at the beginning of this python file by default
+    @return: None
     """
-    conn = sqlite3.connect("sqlite.db")
+    conn = sqlite3.connect(database)
     c = conn.cursor()
     c.execute("INSERT INTO puzzle(puzzlesize) VALUES (?)", (puzzleType,))
     for _, row in currentPuzzle.iterrows():
-        c.execute("""
-                INSERT INTO words(puzzleID, word, IS_VERTICAL, x, y) 
-                VALUES (?, ?, ?, ?, ?)
-            """, (puzzleType, row['word'], row['IS_VERTICAL'], row['x'], row['y']))
+        c.execute("INSERT INTO words(puzzleID, word, IS_VERTICAL, x, y) VALUES (?, ?, ?, ?, ?)",
+                  (puzzleType, row['word'], row['IS_VERTICAL'], row['x'], row['y']))
     conn.commit()
     conn.close()
 
