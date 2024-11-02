78d748ee8576bfb2305e4203541573d109ef9fc5
Adrien Gueguen <adrien.gueguen@etudiant.univ-lr.fr>
2024-11-02 17:07:02 +0100

    "Updated for new Database structure"

--- puzzleGenerator/WordsDatabaseGenerator.py
+++ puzzleGenerator/WordsDatabaseGenerator.py
@@ -9,7 +9,8 @@ def loadDictionary(dictionaryFile:str) -> pd.DataFrame:
     @param dictionaryFile: file path of the dictionary file
     @return: pandas Dataframe containing all the available words
     """
-    return pd.read_csv(dictionaryFile, names=['word'])
+    df = pd.read_csv(dictionaryFile, names=['word'])
+    return df.dropna(subset = ['word'])
 
 def loadFrequency(frequencyFile: str, freqColumns: list) -> pd.DataFrame:
     """
@@ -21,17 +22,17 @@ def loadFrequency(frequencyFile: str, freqColumns: list) -> pd.DataFrame:
 
     frequencies = pd.read_csv(frequencyFile, delimiter='\t', usecols=freqColumns)
     frequencies.rename(columns={freqColumns[0]: 'word'}, inplace=True)
-    frequencies['freq'] = frequencies[freqColumns[1:]].mean(axis=1) # creates freq column as the average of all other columns
+    frequencies['frequency'] = frequencies[freqColumns[1:]].mean(axis=1) # creates freq column as the average of all other columns
 
     frequencies = frequencies[frequencies.word.map(lambda x: isinstance(x, str))] #filter the frequency dataframe to only contain strings in the word column
     frequencies = frequencies[frequencies.word.map(lambda x: x.isalpha())]  # filter the frequency dataframe to only contain alphabetical strings
     frequencies['word'] = frequencies['word'].apply(lambda x: unidecode(x)) # normalize the words to remove accents (unidecode also works for more complex cases, for example: chinese characters)
     frequencies['word'] = frequencies['word'].apply(lambda x: x.upper()) # Capitalizes the words
 
-    frequencies = frequencies.sort_values(by='freq', ascending=False) # Sort by descending frequency
+    frequencies = frequencies.sort_values(by='frequency', ascending=False) # Sort by descending frequency
     frequencies = frequencies.drop_duplicates(subset='word', keep='first') #Remove duplicate words with lower frequency
 
-    return frequencies[['word', 'freq']]
+    return frequencies[['word', 'frequency']]
 
 def loadLanguage(dictionary:pd.DataFrame, frequencies:pd.DataFrame) -> pd.DataFrame:
     """
@@ -42,22 +43,28 @@ def loadLanguage(dictionary:pd.DataFrame, frequencies:pd.DataFrame) -> pd.DataFr
     @return: language Dataframe containing all words and their frequency
     """
 
-    output = pd.merge(dictionary, frequencies[['word', 'freq']], on='word', how='left')
+    output = pd.merge(dictionary, frequencies[['word', 'frequency']], on='word', how='left')
 
-    output = output.sort_values(by='freq', ascending=False).reset_index(drop=True)
+    output = output.sort_values(by='frequency', ascending=False).reset_index(drop=True)
 
     return output
 
-def addLanguageToDatabase(languageName:str, language:pd.DataFrame, databaseName: str = 'puzzles.db') -> None:
+def addLanguageToDatabase(languageName:str, language:pd.DataFrame, databaseName: str, languageCode:str = "") -> None:
     """
     adds language to sqlite database
-    @param languageName: name of the language, becomes name of the table in the database, typically first two characters of language name
+    @param languageName: name of the language, typically first two characters of language name
     @param language: dataframe resulting from loadLanguage
     @param databaseName: name of the sqlite database, typically 'puzzles.db'
+    @param languageCode: optional parameter to specify the language code if it is not the first two letters Capitalized
     @return: None
     """
+    if languageCode == "":
+        languageCode = languageName[:2].upper()
     conn = sqlite3.connect(databaseName)
-    print(language.to_sql(languageName, conn, if_exists='fail'))
+    c = conn.cursor()
+    c.execute("INSERT INTO languages(code, name) SELECT ?, ? WHERE (SELECT 1 FROM languages WHERE code = ?) == NULL ", (languageCode, languageName, languageCode))
+    language['language'] = languageCode
+    print(language.to_sql('words', conn, index=False ,if_exists='append'))
     conn.commit()
     conn.close()
 
